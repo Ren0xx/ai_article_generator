e592a6e5efc4d8a1fff4e0462c881a7e09901a75
Ren0xx <adamzaleski00@gmail.com>
2024-11-14 21:50:36 +0100

    Changing prompt content

--- helpers.js
+++ helpers.js
@@ -8,7 +8,16 @@ function readFile() {
 }
 const fileContent = readFile();
 
-const prompt = `Write me a test html code based on this text: ${fileContent}`;
+const promptContent = ` Please generate HTML content with well-structured text and appropriate topic sections. 
+Use appropriate HTML tags such as headings (<h1>, <h2>, etc.), paragraphs (<p>), and lists to divide and highlight different sections of content
+Add <img> tags with the src="image_placeholder.jpg" attribute in places where the content could be better understood with the help of images. 
+In each <img> tag, include a detailed description of the image in the alt attribute, which can be used to generate the image by the AI.
+Also add appropriate captions under the images using the <figcaption> tag.
+Other requirements: Do not add CSS or Javascript. The generated code should only contain elements that are to be placed inside <body> </body> tags.
+Do not include <html>, <head>, or <body> tags.
+This is the content on which you should rely on:`;
+
+const prompt = `${promptContent} ${fileContent}`;
 
 export { prompt, messageForSystem };
 
